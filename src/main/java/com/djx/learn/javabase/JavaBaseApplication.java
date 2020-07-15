11afcec98b5f1344477d582cb75b3b2e32ae3910
dongjingxi <dongjingxi@ruigushop.com>
2020-07-15 17:28:04 +0800

    init

---
+++ src/main/java/com/djx/learn/javabase/JavaBaseApplication.java
@@ -0,0 +1,13 @@
+package com.djx.learn.javabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBaseApplication.class, args);
+    }
+
+}
